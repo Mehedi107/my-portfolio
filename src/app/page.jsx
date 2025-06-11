62e7964ebaa10e3a2486672e1350c10d4ef8b093
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-06-12 00:59:00 +0600

    Fix: Case-sensitive import path for deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
+import BannerSection from '@/components/sections/banner';
 import AboutSection from '@/components/sections/about';
 import ProjectSection from '@/components/sections/projects';
 import SkillSection from '@/components/sections/skills';
 import ContactSection from '@/components/sections/contact';
 import { ScrollBasedVelocity } from '@/components/custom/scroll-velocity-text';
-import BannerSection from '@/components/sections/banner';
 
 export default function Home() {
   return (
